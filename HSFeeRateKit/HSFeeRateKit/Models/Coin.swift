397908223abc3ff7bd263bbc782bd8feea652b44
Ermat <ealymbaev@gmail.com>
2019-04-12 12:13:52 +0600

    Convert default gas price values to Wei

--- HSFeeRateKit/HSFeeRateKit/Models/Coin.swift
+++ HSFeeRateKit/HSFeeRateKit/Models/Coin.swift
@@ -9,7 +9,7 @@ enum Coin: String, CaseIterable {
         switch self {
         case .bitcoin:      return FeeRate(coin: self, lowPriority: 20, mediumPriority: 40, highPriority: 80, date: Date(timeIntervalSince1970: 1543211299))
         case .bitcoinCash:  return FeeRate(coin: self, lowPriority: 1, mediumPriority: 3, highPriority: 5, date: Date(timeIntervalSince1970: 1543211299))
-        case .ethereum:     return FeeRate(coin: self, lowPriority: 13, mediumPriority: 16, highPriority: 19, date: Date(timeIntervalSince1970: 1543211299))
+        case .ethereum:     return FeeRate(coin: self, lowPriority: 13_000_000_000, mediumPriority: 16_000_000_000, highPriority: 19_000_000_000, date: Date(timeIntervalSince1970: 1543211299))
         }
     }
 
