3bdaa4c0912c38d0025128daf47a0ccfc67d51da
max <trickster77777@gmail.com>
2019-11-21 14:33:37 +0600

    Extract network error in own class

--- FeeRateKit/FeeRateKit/FeeRateKit.swift
+++ FeeRateKit/FeeRateKit/FeeRateKit.swift
@@ -89,3 +89,14 @@ extension FeeRateKit {
     }
 
 }
+
+extension FeeRateKit {
+
+    public enum NetworkError: Error {
+        case invalidUrl
+        case mappingError
+        case noConnection
+        case serverError(status: Int, data: Any?)
+    }
+
+}
