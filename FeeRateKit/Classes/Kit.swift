44a9da96067fea69f4c21088f3d00773265d1c22
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2021-01-20 13:13:50 +0600

    Remove FeeRate class. Kit should provide single rates
     - Remove cache
     - Set fixed rates for LTC, DASH and BCH

--- FeeRateKit/Classes/Kit.swift
+++ FeeRateKit/Classes/Kit.swift
@@ -2,79 +2,36 @@ import HsToolKit
 import RxSwift
 
 public class Kit {
-    private let cache: IStorage
-    private let providerManager: FeeRateProviderManager
+    private let btcCoreProvider: BtcCoreProvider
+    private let infuraProvider: InfuraProvider
 
-    init(storage: IStorage, providerManager: FeeRateProviderManager) {
-        self.cache = storage
-        self.providerManager = providerManager
-    }
-
-    private func getRate(coin: Coin) -> Single<FeeRate> {
-        providerManager.getFeeRateProvider(coin: coin).getFeeRates()
-    }
-
-    private func name(from coin: Coin) -> String {
-        switch coin {
-        case .bitcoin: return "Bitcoin"
-        case .litecoin: return "Litecoin"
-        case .bitcoinCash: return "Bitcoin Cash"
-        case .dash: return "Dash"
-        case .ethereum: return "Ethereum"
-        }
-    }
-
-    private func tupleSingle(from coin: Coin) -> Single<(String, Any)> {
-        getRate(coin: coin)
-                .flatMap { [unowned self] rate -> Single<(String, Any)> in
-                    .just((self.name(from: coin), [
-                        ("low", rate.low),
-                        ("medium", rate.medium),
-                        ("high", rate.high),
-                        ("date", rate.date)
-                    ]))
-                }
-                .catchError { [unowned self] error in
-                    .just(("\(self.name(from: coin)) error", error.localizedDescription))
-                }
+    init(btcCoreProvider: BtcCoreProvider, infuraProvider: InfuraProvider) {
+        self.btcCoreProvider = btcCoreProvider
+        self.infuraProvider = infuraProvider
     }
 
 }
 
 extension Kit {
 
-    public var bitcoin: Single<FeeRate> {
-        getRate(coin: .bitcoin)
+    public func bitcoin(blockCount: Int) -> Single<Int> {
+        btcCoreProvider.getFeeRate(blockCount: blockCount)
     }
 
-    public var litecoin: Single<FeeRate> {
-        getRate(coin: .litecoin)
+    public var litecoin: Single<Int> {
+        Single.just(1)
     }
 
-    public var bitcoinCash: Single<FeeRate> {
-        getRate(coin: .bitcoinCash)
+    public var bitcoinCash: Single<Int> {
+        Single.just(3)
     }
 
-    public var dash: Single<FeeRate> {
-        getRate(coin: .dash)
+    public var dash: Single<Int> {
+        Single.just(4)
     }
 
-    public var ethereum: Single<FeeRate> {
-        getRate(coin: .ethereum)
-    }
-
-    public func getRate(coinCode: String) -> Single<FeeRate> {
-        if let  coin = Coin(rawValue: coinCode) {
-            return getRate(coin: coin)
-        }
-
-        return .error(FeeRateError.coinNotAvailable)
-    }
-
-    public func statusInfo() -> Single<[(String, Any)]> {
-        let coins: [Coin] = [.bitcoin, .litecoin, .bitcoinCash, .dash, .ethereum]
-        let tupleSingles: [Single<(String, Any)>] = coins.map(tupleSingle)
-        return Single.zip(tupleSingles)
+    public var ethereum: Single<Int> {
+        infuraProvider.getFeeRate()
     }
 
 }
@@ -84,12 +41,12 @@ extension Kit {
     public static func instance(providerConfig: FeeProviderConfig, minLogLevel: Logger.Level = .error) -> Kit {
         let logger = Logger(minLogLevel: minLogLevel)
 
-        let cache: IStorage = FeeRateCache()
         let networkManager = NetworkManager(logger: logger)
 
-        let feeRateProvider = FeeRateProviderManager(providerConfig: providerConfig, networkManager: networkManager, cache: cache)
+        let btcCoreProvider = BtcCoreProvider(networkManager: networkManager, config: providerConfig)
+        let infuraProvider = InfuraProvider(networkManager: networkManager, config: providerConfig)
 
-        let kit = Kit(storage: cache, providerManager: feeRateProvider)
+        let kit = Kit(btcCoreProvider: btcCoreProvider, infuraProvider: infuraProvider)
 
         return kit
     }
